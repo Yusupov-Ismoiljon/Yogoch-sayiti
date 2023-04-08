303b93e2907f947d9cc7a7b0a0735a833eadd947
iy2008iy <yusupovismoliljan1204@gmail.com>
2023-04-08 14:55:21 +0500

    start

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: [
+    "./*.{html,js}",
+    "./node_modules/flowbite/**/*.js"
+],
   theme: {
     extend: {
       maxWidth:{
         maxw:'1480px',
+        main_max_w: '1203px',
+        sev: '1018px',
+        with: '646px',
         hero_max_w: '539px',
         link_max_w: '240px',
+        max_w_mwtext: '420px',
         max_w: '175px',
-        max_w_mwtext: '375px',
-        main_max_w: '1203px'
+
       },
       colors: {
         address_black: '#222',  
         p: '#777',
         grey_bg: '#FF9B04',
-        cd: '#CDCDCD' 
+        cd: '#CDCDCD',
+        yellow: '#FF9B04', 
       },
       backgroundColor: {
         grey_bg: '#F5F5F5',
+        gray: '#F5F5F5',
         bg_f9: '#FF9B04',
         yellow: '#FF9B04',
         cd: '#CDCDCD',
@@ -28,5 +36,7 @@ module.exports = {
       }
     }
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
