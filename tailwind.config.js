29ae4bccc119d8921ccb9f5f3753c0557102ccbf
iyiy2008 <yusupovismoiljon1204@gmail.com>
2023-04-18 14:30:46 +0500

    init col

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,22 +19,29 @@ module.exports = {
         gboz: '366.42px',
         aksiya: '345px',
         link_max_w: '240px',
-        
         catalogmw: '215px',
         max_w: '175px',
 
       },
       width: {
-        img: '460px',
+        div: '1075px',
         div_img: '650px',
+        divide: '520px',
+        img: '470px',
+        mm: '315px',
+        ink_1: '255px',
         link: '250px',
+        do: '190px',
+        img1: '82px',
+
       },
       colors: {
         address_black: '#222',  
         p: '#777',
         grey_bg: '#FF9B04',
         cd: '#CDCDCD',
-        yellow: '#FF9B04', 
+        yellow: '#FF9B04',
+        s8: '#888',
       },
       backgroundColor: {
         grey_bg: '#F5F5F5',
@@ -43,6 +50,8 @@ module.exports = {
         yellow: '#FF9B04',
         cd: '#CDCDCD',
         green: '#31C76D',
+        f8: '#F8F8F8',
+        
       },
       boxShadow: {
         show: '0-4-10-#000000-5%'
